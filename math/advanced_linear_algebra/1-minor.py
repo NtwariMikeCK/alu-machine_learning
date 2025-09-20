5ea6d9e5dc0bf9a75997c16e001940e39b9530eb
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2025-09-20 12:14:04 +0200

    Update 1-minor.py

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -4,6 +4,27 @@ dule to calculate the minor matrix of a square matrix
 """
 
 
+def determinant(matrix):
+    """
+    Recursively calculates the determinant of a square matrix
+    """
+    if matrix == [[]]:
+        return 1
+
+    n = len(matrix)
+    if n == 1:
+        return matrix[0][0]
+    if n == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    det = 0
+    for col in range(n):
+        submatrix = [row[:col] + row[col + 1:] for row in matrix[1:]]
+        det += ((-1) ** col) * matrix[0][col] * determinant(submatrix)
+    return det
+
+
+
 def minor(matrix):
     """
     Calculates the minor matrix of a square matrix
