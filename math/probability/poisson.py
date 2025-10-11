f6eb6791f7f81f89f11f77945c3576e30dc6866c
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2025-10-11 15:11:51 -0700

    Update poisson.py

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -2,7 +2,7 @@
 """
 Poisson distribution class
 """
-import math
+
 
 class Poisson:
     """
@@ -31,35 +31,3 @@ class Poisson:
 
             # Calculate lambtha as mean of data
             self.lambtha = float(sum(data) / len(data))
-
-    def pmf(self, k):
-        """
-        Calculates the PMF value for a given number of “successes” (k)
-        PMF(k; λ) = (e^(-λ) * λ^k) / k!
-        """
-        if not isinstance(k, int):
-            k = int(k)
-        if k < 0:
-            return 0
-
-        # Compute PMF
-        e_term = math.exp(-self.lambtha)
-        fact_term = math.factorial(k)
-        pmf = (e_term * (self.lambtha ** k)) / fact_term
-        return pmf
-    
-    def cdf(self, k):
-        """
-        Calculates the CDF value for a given number of “successes”
-        CDF(k; λ) = sum_{i=0}^{k} PMF(i; λ)
-        """
-        if not isinstance(k, int):
-            k = int(k)
-        if k < 0:
-            return 0
-
-        cdf = 0
-        for i in range(k + 1):
-            cdf += self.pmf(i)
-        return cdf
-    
