d2cf62fb7147f0163ad192966c93a03bd7857ac1
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2025-10-11 15:24:35 -0700

    Update poisson.py

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -28,7 +28,7 @@ class Poisson:
             self.lambtha = float(sum(data) / len(data))
 
     def _factorial(self, n):
-        """Compute factorial of n without importing math"""
+        """Compute factorial of n without math"""
         if n == 0 or n == 1:
             return 1
         result = 1
