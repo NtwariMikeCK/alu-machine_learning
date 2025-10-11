cbab5783b8999c537330cad20ba7fdda91363945
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2025-10-11 15:52:12 -0700

    Update exponential.py

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -27,3 +27,34 @@ class Exponential:
             # For exponential distribution, lambtha is 1 / mean
             mean = sum(data) / len(data)
             self.lambtha = float(1 / mean)
+
+    def _exp(self, x, terms=100):
+        """Compute e^x using Taylor series expansion"""
+        result = 1.0
+        term = 1.0
+        for i in range(1, terms):
+            term *= x / i
+            result += term
+        return result
+
+    def pdf(self, x):
+        """
+        Calculates the PDF value for a given time period x
+        PDF(x; λ) = λ * e^(-λ * x)
+
+        Returns 0 if x < 0
+        """
+        if x < 0:
+            return 0
+        return self.lambtha * self._exp(-self.lambtha * x)
+
+    def cdf(self, x):
+        """
+        Calculates the CDF value for a given time period x
+        CDF(x; λ) = 1 - e^(-λ * x)
+
+        Returns 0 if x < 0
+        """
+        if x < 0:
+            return 0
+        return 1 - self._exp(-self.lambtha * x)
