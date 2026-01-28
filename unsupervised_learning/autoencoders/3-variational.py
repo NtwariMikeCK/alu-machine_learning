a054c778d3b45378fc466b9898c16f91adbafbf0
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2026-01-28 13:19:03 +0200

    Update 3-variational.py

--- unsupervised_learning/autoencoders/3-variational.py
+++ unsupervised_learning/autoencoders/3-variational.py
@@ -70,5 +70,4 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
         return xent_loss + kl_loss
 
     auto.compile(optimizer='adam', loss=vae_loss)
-
     return encoder, decoder, auto
